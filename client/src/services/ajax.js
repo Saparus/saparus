e8688a31de61f31fd5d2a68bcd0c682c31a518a4
GiorgiVartanov <minisk50@gmail.com>
2024-12-22 17:31:20 +0400

    fix: simplify CORS configuration for API endpoints

--- client/src/services/ajax.js
+++ client/src/services/ajax.js
@@ -8,6 +8,8 @@ const ajax = axios.create({
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
   },
 })
 
