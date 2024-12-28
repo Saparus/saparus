cf6d1271b6d85c1488825e7c1325505ea4c0e96b
GiorgiVartanov <minisk50@gmail.com>
2024-12-28 21:30:59 +0400

    fix: update table name in authorize function to use USERS_TABLE

--- server/src/handlers/auth/authorize.mjs
+++ server/src/handlers/auth/authorize.mjs
@@ -11,7 +11,7 @@ export const authorize = async (event, context, callback) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
     const params = {
-      TableName: process.env.USER_TABLE,
+      TableName: process.env.USERS_TABLE,
       Key: { id: decoded.id },
     }
 
