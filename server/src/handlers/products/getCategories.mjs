0f42bb52487b0957fc41da760e41679a20f2e398
GiorgiVartanov <minisk50@gmail.com>
2024-12-29 20:27:43 +0400

    fix: change import of summarizeCategoryData to include file extension

--- server/src/handlers/products/getCategories.mjs
+++ server/src/handlers/products/getCategories.mjs
@@ -1,7 +1,7 @@
 import { ScanCommand } from "@aws-sdk/lib-dynamodb"
 
 import { db } from "../../util/db.mjs"
-import { summarizeCategoryData } from "../../util/summarizeCategoryData"
+import { summarizeCategoryData } from "../../util/summarizeCategoryData.mjs"
 
 export const getCategories = async (event) => {
   const params = {
@@ -14,17 +14,6 @@ export const getCategories = async (event) => {
     const { Items: products } = await db.send(scanCommand)
 
     const categories = summarizeCategoryData(products)
-
-    if (!categories || categories.length === 0) {
-      return {
-        statusCode: 404,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-        body: JSON.stringify({ message: "Categories not found" }),
-      }
-    }
 
     return {
       statusCode: 200,
