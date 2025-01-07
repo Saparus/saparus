9728320d40e2971071cd5369c5d46bdcad849021
GiorgiVartanov <minisk50@gmail.com>
2025-01-07 19:29:43 +0400

    fix: add package to getProduct in serverless.yml

--- server/src/handlers/products/getProduct.mjs
+++ server/src/handlers/products/getProduct.mjs
@@ -1,6 +1,6 @@
 import { GetCommand } from "@aws-sdk/lib-dynamodb"
 
-import { db } from "../../../util/db.mjs"
+import { db } from "../../util/db.mjs"
 
 export const getProduct = async (event) => {
   const { language } = event.queryStringParameters
